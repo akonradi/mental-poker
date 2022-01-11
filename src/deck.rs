7b5703203003cfa5b6d1f1256dcad9c9bcc0af71
Alex Konradi <alexkonradi@gmail.com>
2022-01-11 08:29:43 -0500

    Add types for deck and cards
    Add basic types to represent a fixed-sized set of cards, references to
    positions within a (shuffled) deck, and the value of a card.

---
+++ src/deck.rs
@@ -0,0 +1,153 @@
+use std::marker::PhantomData;
+
+/// The type of a deck.
+///
+/// Implementations of this trait define the type of a deck. A deck has `SIZE`
+/// cards, all unique. Types that implement `DeckType` do not need to be
+/// instantiatable; they are used only as markers.
+pub trait DeckType:
+    std::fmt::Debug + Copy + Clone + Eq + PartialEq + PartialOrd + std::hash::Hash
+{
+    const SIZE: usize;
+}
+
+/// The value of a card in a deck.
+///
+/// A `Card<D>` represents the unique value of a card that is a member of `D:
+/// DeckType`. Cards within a deck have values in the range [0, D::SIZE-1].
+/// Custom traits can be used to attach additional information to a card, like
+/// "suit" or "rank", derived from the card's index.
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
+pub struct Card<DeckType>(u16, PhantomData<DeckType>);
+
+/// A position within a shuffled deck of cards.
+///
+/// A `DeckPosition<D>` represents an index into a shuffled list of cards who
+/// are members of a deck `D : DeckType`.
+#[derive(derivative::Derivative)]
+#[derivative(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
+pub struct DeckPosition<DeckType>(usize, PhantomData<DeckType>);
+
+impl<D: DeckType> Card<D> {
+    /// Creates a new card with the given value.
+    pub(crate) fn new(index: u16) -> Self {
+        Self(index, PhantomData)
+    }
+
+    /// Returns the set of cards in the deck in order by value.
+    pub fn all() -> [Self; D::SIZE]
+    where
+        [(); D::SIZE]:,
+    {
+        let mut cards = [Self::new(0); D::SIZE];
+        for (i, c) in cards.iter_mut().enumerate() {
+            c.0 = i.try_into().unwrap();
+        }
+        cards
+    }
+}
+
+impl<D: DeckType> DeckPosition<D> {
+    const MAX: Self = DeckPosition(D::SIZE - 1, PhantomData);
+
+    /// Returns the position of the first card in the shuffled deck.
+    pub fn start() -> Self {
+        Self(0, PhantomData)
+    }
+
+    /// Returns a range over all positions in the shuffled deck.
+    pub fn positions() -> std::ops::RangeInclusive<Self> {
+        Self::start()..=Self::MAX
+    }
+}
+
+impl<D: DeckType> From<Card<D>> for u16 {
+    fn from(c: Card<D>) -> u16 {
+        c.0
+    }
+}
+
+impl<D: DeckType> From<&'_ Card<D>> for u16 {
+    fn from(c: &Card<D>) -> u16 {
+        c.0
+    }
+}
+
+impl<D> From<DeckPosition<D>> for usize {
+    fn from(pos: DeckPosition<D>) -> Self {
+        pos.0
+    }
+}
+
+impl<D> From<&'_ DeckPosition<D>> for usize {
+    fn from(pos: &DeckPosition<D>) -> Self {
+        pos.0
+    }
+}
+
+impl<D: DeckType> std::iter::Step for DeckPosition<D> {
+    fn steps_between(start: &Self, end: &Self) -> Option<usize> {
+        usize::steps_between(&start.0, &end.0)
+    }
+
+    fn forward_checked(start: Self, count: usize) -> Option<Self> {
+        usize::forward_checked(start.0, count).and_then(|index| {
+            if index < D::SIZE {
+                Some(Self(index, PhantomData))
+            } else {
+                None
+            }
+        })
+    }
+
+    fn backward_checked(start: Self, count: usize) -> Option<Self> {
+        usize::backward_checked(start.0, count).and_then(|index| {
+            if index < D::SIZE {
+                Some(Self(index, PhantomData))
+            } else {
+                None
+            }
+        })
+    }
+
+    fn forward(start: Self, count: usize) -> Self {
+        Self::forward_checked(start, count).unwrap()
+    }
+
+    unsafe fn forward_unchecked(start: Self, count: usize) -> Self {
+        Self(start.0 + count, PhantomData)
+    }
+
+    fn backward(start: Self, count: usize) -> Self {
+        Self::backward_checked(start, count).unwrap()
+    }
+
+    unsafe fn backward_unchecked(start: Self, count: usize) -> Self {
+        Self(start.0 - count, PhantomData)
+    }
+}
+
+impl<T, D: DeckType> std::ops::Index<DeckPosition<D>> for [T; D::SIZE] {
+    type Output = T;
+    fn index(&self, position: DeckPosition<D>) -> &Self::Output {
+        &self[position.0]
+    }
+}
+
+impl<T, D: DeckType> std::ops::IndexMut<DeckPosition<D>> for [T; D::SIZE] {
+    fn index_mut(&mut self, position: DeckPosition<D>) -> &mut Self::Output {
+        &mut self[position.0]
+    }
+}
+
+impl<D> std::fmt::Debug for Card<D> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Card").field(&self.0).finish()
+    }
+}
+
+impl<D> std::fmt::Debug for DeckPosition<D> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("DeckPosition").field(&self.0).finish()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+#![allow(incomplete_features)]
+#![feature(step_trait)]
+#![feature(generic_const_exprs)]
+
+pub(crate) mod deck;
+
+use derive_more::From;
+
+pub use deck::{Card, DeckPosition, DeckType};
+
+/// An identifier used to uniquely identify another player.
+///
+/// This identifier is locally scoped, and meaningful only in the context of the
+/// [`game::CardGame`] instance that creates it.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct OtherPlayerId(u8);
+
+impl OtherPlayerId {
+    /// Constructs a new ID.
+    #[cfg(test)]
+    pub(crate) fn new(id: u8) -> Self {
+        Self(id)
+    }
+}
+
+/// An identifier use to uniquely identify a player.
+///
+/// This identifier is locally scoped, and meaningful only in the context of the
+/// [`game::CardGame`] instance that creates it.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, From)]
+pub enum PlayerId {
+    /// The "local" player.
+    This,
+    /// Some other player.
+    Other(OtherPlayerId),
+}
+
+impl From<&'_ OtherPlayerId> for u8 {
+    fn from(id: &OtherPlayerId) -> u8 {
+        id.0
+    }
+}
+
+impl From<OtherPlayerId> for u8 {
+    fn from(id: OtherPlayerId) -> u8 {
+        id.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn other_player_id_unique() {
+        let a = OtherPlayerId::new(8);
+        let b = OtherPlayerId::new(91);
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    fn other_player_id_into() {
+        assert_eq!(5u8, OtherPlayerId::new(5).into());
+        assert_eq!(5u8, (&OtherPlayerId::new(5)).into());
+    }
+
+    #[test]
+    fn player_id_unique() {
+        let a = OtherPlayerId::new(8).into();
+        let b = OtherPlayerId::new(91).into();
+        let t = PlayerId::This;
+        assert_ne!(t, a);
+        assert_ne!(t, b);
+        assert_ne!(a, b);
+    }
+}
